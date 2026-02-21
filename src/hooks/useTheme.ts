ea09e4a71eea3df02b561662bf63c1d8c5f1b3c1
prachiRemote <prachi.jain@remotestate.in>
2026-02-22 01:02:40 +0530

    created hook for theme

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState<"dark" | "light">(() => {
+    return (localStorage.getItem("theme") as "dark" | "light") || "dark";
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if (theme === "light") {
+      root.classList.add("light");
+    } else {
+      root.classList.remove("light");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return { theme, setTheme };
+};
